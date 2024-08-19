ffadbb19ee71ae75569fef05f08aaac844566076
chans <imagechans@gmail.com>
2024-08-19 14:44:36 +0800

    update code

--- query_server/transfer/filters.py
+++ query_server/transfer/filters.py
@@ -3,16 +3,18 @@ from .models import D9Transfer, USDTTransfer
 
 
 class D9TransferFilter(filters.FilterSet):
-    from_address = filters.CharFilter(field_name='data__from', lookup_expr='icontains')
+    from_address = filters.CharFilter(field_name='data__from', lookup_expr='contains')
+    to_address = filters.CharFilter(field_name='data__to', lookup_expr='contains')
 
     class Meta:
         model = D9Transfer
-        fields = ['from_address', 'block_number', 'timestamp', 'extrinsic_hash', 'contract', 'contract_address', 'kind']
+        fields = ['from_address', 'to_address','block_number', 'timestamp', 'extrinsic_hash', 'contract', 'contract_address', 'kind']
 
 
 class USDTTransferFilter(filters.FilterSet):
-    from_address = filters.CharFilter(field_name='data__from', lookup_expr='icontains')
+    from_address = filters.CharFilter(field_name='data__from', lookup_expr='contains')
+    to_address = filters.CharFilter(field_name='data__to', lookup_expr='contains')
 
     class Meta:
         model = USDTTransfer
-        fields = ['from_address', 'block_number', 'timestamp', 'extrinsic_hash', 'contract', 'contract_address', 'kind']
+        fields = ['from_address', 'to_address', 'block_number', 'timestamp', 'extrinsic_hash', 'contract', 'contract_address', 'kind']
